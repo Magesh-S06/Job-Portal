ce545241e7ba538e7b4f86fc2ef27a030ff1ff48
Magesh-S06 <magesh04.siva@gmail.com>
2025-08-27 20:40:29 +0530

    Latest commit

---
+++ server/routes/companyRoutes.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import { changeJobApplicationsStatus, changeVisibility, getCompanyData, getCompanyJobApplicants, getCompanyPostedJobs, loginCompany, postJob, registerCompany } from '../controller/company.controller.js'
+import upload from '../config/multer.js'
+import { protectCompany } from '../middlewares/auth.js'
+
+const router = express.Router()
+
+router.post('/register',upload.single('image'), registerCompany)
+
+router.post('/login',loginCompany)
+
+router.get('/company',protectCompany, getCompanyData)
+
+router.post('/post-job',protectCompany, postJob)
+
+router.get('/applicants',protectCompany, getCompanyJobApplicants)
+
+router.get('/list-jobs',protectCompany, getCompanyPostedJobs)
+
+router.post('/change-status',protectCompany, changeJobApplicationsStatus)
+
+router.post('change-visibility',protectCompany, changeVisibility)
+
+export default router
--- server/server.js
+++ server/server.js
@@ -5,10 +5,14 @@ import dotenv from "dotenv"
 import {connectDB} from "./config/database.js"
 import * as Sentry from "@sentry/node"
 import { clerkWebhooks } from "./controller/webhooks.controller.js"
+import companyRoutes from './routes/companyRoutes.js'
+import connectCloudinary from "./config/cloudinary.js"
 
 dotenv.config()
 
 const app = express()
+
+await connectCloudinary()
 
 app.use(cors())
 app.use(express.json())
@@ -19,7 +23,10 @@ app.get("/",(req,res)=>
 app.get("/debug-sentry", function mainHandler(req, res) {
   throw new Error("My first Sentry error!");
 });
+
 app.post('/webhooks',clerkWebhooks)
+
+app.use('/api/company',companyRoutes)
 
 const PORT = process.env.PORT || 5000
 
