5d1bf32ba76b4c5a525ae3f0f7d9a618d5103777
Magesh-S06 <magesh04.siva@gmail.com>
2025-08-29 12:43:31 +0530

    Backend commit

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ import { clerkWebhooks } from "./controller/webhooks.controller.js"
 import companyRoutes from './routes/companyRoutes.js'
 import connectCloudinary from "./config/cloudinary.js"
 import jobRoutes from './routes/jobRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import {clerkMiddleware} from '@clerk/express'
 
 dotenv.config()
 
@@ -17,6 +19,7 @@ await connectCloudinary()
 
 app.use(cors())
 app.use(express.json())
+app.use(clerkMiddleware())
 
 app.get("/",(req,res)=>
     res.send("API Working")
@@ -28,6 +31,7 @@ app.get("/debug-sentry", function mainHandler(req, res) {
 app.post('/webhooks',clerkWebhooks)
 app.use('/api/company',companyRoutes)
 app.use('/api/jobs',jobRoutes)
+app.use('/api/users',userRoutes)
 
 const PORT = process.env.PORT || 5000
 
