439f4d098488f45469d1b423ef3b349c7768e92c
Magesh-S06 <magesh04.siva@gmail.com>
2025-09-08 19:33:36 +0530

    Live commit

--- client/src/pages/Applications.jsx
+++ client/src/pages/Applications.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import NavBar from '../components/Navbar'
-import { assets, jobsApplied } from '../assets/assets'
+import { assets } from '../assets/assets'
 import moment from 'moment'
 import Footer from '../components/Footer'
 import { useAppContext } from '../context/AppContext'
@@ -61,10 +61,10 @@ const Applications = () => {
             <button onClick={updateResume} className='bg-green-100 border border-green-400 rounded-lg px-4 py-2 cursor-pointer'>Save</button>
           </>:
           <div className='flex gap-2'>
-            <a target='_blank' href={userData.resume} className='bg-blue-100 text-blue-600 px-4 py-2 rounded-lg'>
+            <a target='_blank' href={userData?.resume} className='bg-blue-100 text-blue-600 px-4 py-2 rounded-lg'>
               Resume
               </a> 
-              <button onClick={updateResume} className='text-gray-500 border border-gray-300 rounded-lg px-4 py-2 cursor-pointer'>
+              <button onClick={()=>setIsEdit(true)} className='text-gray-500 border border-gray-300 rounded-lg px-4 py-2 cursor-pointer'>
                 Edit
               </button>
           </div>
