28593fbf33e3089ca422926b919c897b1a50e54b
Magesh-S06 <magesh04.siva@gmail.com>
2026-03-26 10:35:51 +0530

    email commit

--- server/utils/sendJobMail.js
+++ server/utils/sendJobMail.js
@@ -3,11 +3,14 @@ import nodemailer from "nodemailer";
 // dotenv.config()
 
 const transporter = nodemailer.createTransport({
-  service: "gmail", // or use custom SMTP later
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 10000,
 });
 
 export const sendJobNotification = async (job, emails) => {
